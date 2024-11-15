2e9c75c2ad5acf375d4248c39e691afe7e79e75f
jammieeeeee <lijiayijamie@gmail.com>
2024-11-15 16:47:29 +0800

    edits

--- SC2002_Assignment/DoctorAvailabilityCsvHelper.java
+++ SC2002_Assignment/DoctorAvailabilityCsvHelper.java
@@ -78,5 +78,15 @@ public class DoctorAvailabilityCsvHelper {
             return false;
         }
     }
+
+    public static DoctorModel getDoctorById(String doctorID) {
+        List<DoctorModel> doctors = loadDoctors();
+        for (DoctorModel doctor : doctors) {
+            if (doctor.getHospitalID().equals(doctorID)) {
+                return doctor;
+            }
+        }
+        return null;
+    }
     
 }
