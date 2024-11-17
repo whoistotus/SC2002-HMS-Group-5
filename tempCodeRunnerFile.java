55e11cca00a6967ecb92c45b2ee3a51e25fd8628
katchinoseyes <kathlyn.chin.weiting@gmail.com>
2024-11-18 00:28:44 +0800

    .

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,3 +1,7 @@
-
-                    DoctorView doctorView = new DoctorView(doctorModel);
+                DoctorModel doctorModel = constructDoctorModel(currentUser.getHospitalID(), password);
+                if (doctorModel != null) {
+                   DoctorView doctorView = new DoctorView(doctorModel);
                     doctorView.DoctorMenu();
+                } else {
+                     System.out.println("Error: Could not find doctor details in the system.");
+                }
