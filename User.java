dbcf0f21600e0663adc56ed8f48f8bf6832ecec5
aniston <159416780+anistontan@users.noreply.github.com>
2024-11-18 13:20:00 +0800

    UserInterface

--- User.java
+++ User.java
@@ -1,27 +1,35 @@
-
-public class User {
+public class User implements UserInterface {
     private String hospitalID;
     private String password;
     private String userRole;
 
+    // Constructor
     public User(String hospitalID, String password, String userRole) {
         this.hospitalID = hospitalID;
-        this.userRole = userRole;
         this.password = password;
+        this.userRole = userRole;
     }
 
+    // Getter for hospital ID
+    @Override
     public String getHospitalID() {
         return hospitalID;
     }
 
+    // Getter for password
+    @Override
     public String getPassword() {
         return password;
     }
 
+    // Getter for user role
+    @Override
     public String getUserRole() {
         return userRole;
     }
 
+    // Setter for password
+    @Override
     public void setPassword(String password) {
         this.password = password;
     }
