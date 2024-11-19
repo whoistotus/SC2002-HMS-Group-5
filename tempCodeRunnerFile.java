7b38824f507f58db5f089c2a74c0d0b095e78d5a
whoistotus <163799739+whoistotus@users.noreply.github.com>
2024-11-19 17:08:05 +0800

    Removing all the test applications

--- tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-                // DoctorModel doctorModel = constructDoctorModel(currentUser.getHospitalID(), password);
-                // if (doctorModel != null) {
-                //    DoctorView doctorView = new DoctorView(doctorModel);
-                //     doctorView.DoctorMenu();
-                // } else {
-                //      System.out.println("Error: Could not find doctor details in the system.");
-                // }
-                // break;
